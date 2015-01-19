bdbcc458d13b72e0aa5573fdbf71676519206871
bliss administrator <blissadm@sybilnew.esrf.fr>
2015-01-19 15:30:10 +0100

    added config file moved code to __init__.py

---
+++ xdsinp/config.py
@@ -0,0 +1,8 @@
+# Configuration for the server. For now only ispyb stuff.
+
+# webservice endpoint
+WS_URL="http://160.103.210.124:8080/ispyb-ejb3/ispybWS/ToolsForCollectionWebService?wsdl"
+# ispyb username
+ISPYB_USER="ispybws1"
+# ...and password
+ISPYB_PASSWORD="!5pybws1"
